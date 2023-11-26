5060c794157977c98107b86e6b999f8246778cba
TeoNemsadze <teona.nemsadze.2@iliauni.edu.ge>
2023-11-26 11:57:07 +0400

    Initial Commit

---
+++ Voyager/VoyagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoyagerApp.swift
+//  Voyager
+//
+//  Created by teona nemsadze on 26.11.23.
+//
+
+import SwiftUI
+
+@main
+struct VoyagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
